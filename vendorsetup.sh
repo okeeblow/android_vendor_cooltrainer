e1f090776b8cf321a33c82322158383f5300022c
Nicole Reid <git@cooltrainer.org>
2014-06-29 15:50:13 -0700

    Fix lunch combos

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,6 @@
 add_lunch_combo cooltrainer_arm-eng
-add_lunch_combo cooltrainer_maguro-eng
-add_lunch_combo cooltrainer_maguro-userdebug
+add_lunch_combo cooltrainer_t0lteatt-eng
+add_lunch_combo cooltrainer_t0lteatt-userdebug
 
 export DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
