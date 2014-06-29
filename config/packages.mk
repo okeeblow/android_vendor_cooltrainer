2c67675f326b2614519a44560270bc590ecc1ba9
Nicole Reid <git@cooltrainer.org>
2014-06-29 15:42:58 -0700

    Import DU PRODUCT_PACKAGES

--- config/packages.mk
+++ config/packages.mk
@@ -15,6 +15,48 @@ PRODUCT_PACKAGES += \
   GmsCore \
   FakeStore
 
+# Additional packages
+PRODUCT_PACKAGES += \
+  Development \
+  LatinIME \
+  VideoEditor \
+  VoiceDialer \
+  SoundRecorder \
+  Basic
+
+# Additional apps
+PRODUCT_PACKAGES += \
+  Apollo \
+  DashClock \
+  DSPManager \
+  DU_about \
+  DU_changelog \
+  DU_walls \
+  libcyanogen-dsp \
+  audio_effects.conf \
+  MonthCalendarWidget \
+  OmniSwitch \
+  Terminal
+
+PRODUCT_PACKAGES += \
+  CellBroadcastReceiver
+
+# Additional tools
+PRODUCT_PACKAGES += \
+  openvpn \
+  e2fsck \
+  mke2fs \
+  tune2fs \
+  bash \
+  vim \
+  nano \
+  htop \
+  powertop \
+  lsof \
+  mount.exfat \
+  fsck.exfat \
+  mkfs.exfat
+
 # libmemtrack is needed e.g. by foursquare
 # looks like it's available on all devices
 PRODUCT_PACKAGES += libmemtrack
