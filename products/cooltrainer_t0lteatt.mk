4e4e93be3195c55552ab468fca7807d7447a8a84
Nicole Reid <git@cooltrainer.org>
2014-06-29 19:55:21 -0700

    Update product info and use AGNi

--- products/cooltrainer_t0lteatt.mk
+++ products/cooltrainer_t0lteatt.mk
@@ -7,10 +7,13 @@ TARGET_ARCH := arm
 ARGET_ARCH_VARIANT := armv7-a-neon
 
 PRODUCT_NAME  := cooltrainer_t0lteatt
-PRODUCT_BRAND := Android
-PRODUCT_MODEL := CoolROM on t0lteatt
+PRODUCT_BRAND := Cooltrainer
+PRODUCT_MANUFACTURER := Cooltrainer
+PRODUCT_MODEL := Galaxy Note II
 PRODUCT_DEVICE := t0lteatt
 PRODUCT_RESTRICT_VENDOR_FILES := false
+
+TARGET_KERNEL_SOURCE := kernel/samsung/smdk4412-agni
 
 PRODUCT_PROPERTY_OVERRIDES += \
   ro.bq.gpu_to_cpu_unsupported=1 \
