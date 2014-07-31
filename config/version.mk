b68ffa8a6605cc8538cbc25195f8edbd36fdf192
Nicole Reid <git@cooltrainer.org>
2014-07-31 15:55:46 -0700

    Standardize build tag

--- config/version.mk
+++ config/version.mk
@@ -2,8 +2,9 @@ TARGET_PRODUCT_SHORT := $(TARGET_PRODUCT)
 TARGET_PRODUCT_SHORT := $(subst cooltrainer_,,$(TARGET_PRODUCT_SHORT))
 
 DATE = $(shell date +%Y%m%d-%H%M%z)
-ROM_VERSION := $(PLATFORM_VERSION)-$(DATE)-$(TARGET_PRODUCT_SHORT)
+ROM_VERSION := Cooltrainer-$(PLATFORM_VERSION)-$(DATE)-$(TARGET_PRODUCT_SHORT)
 
 PRODUCT_PROPERTY_OVERRIDES += \
-  ro.modversion=Cooltrainer-$(ROM_VERSION) \
-  ro.du.version=$(TARGET_PRODUCT)_$(ANDROID_VERSION)_$(DATE)
+  BUILD_DISPLAY_ID=$(BUILD_ID) \
+  ro.modversion=$(ROM_VERSION) \
+  ro.du.version=$(ROM_VERSION)
